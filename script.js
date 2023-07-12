0712852446958b0fcc5657ef6692f6e29e6945e4
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-12 23:54:26 +0530

    Function to check for a win

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 let music = new Audio("music.mp3")
 let audioTurn = new Audio("ting.mp3")
 let gameover = new Audio("gameover.mp3")
-
 let turn = "X"
+let isgameover = false
 
 //Function to change the turn
 const changeTurn = () => {
@@ -12,12 +12,28 @@ const changeTurn = () => {
 
 // Function to check for a win
 const checkWin = () => {
-
+    let boxtexts = document.getElementsByClassName('boxtext')
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    wins.forEach(e => {
+        if ((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[2]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[0]].innerText !== '')) {
+            document.querySelector('.info').innerText = boxtexts[e[0]].innerText + " Win"
+            isgameover = true
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px"
+        }
+    })
 }
 
 // Game Logic
 let boxes = document.getElementsByClassName('box')
-
 Array.from(boxes).forEach((element) => {
     let boxtext = element.querySelector('.boxtext')
     element.addEventListener('click', (e) => {
@@ -26,7 +42,9 @@ Array.from(boxes).forEach((element) => {
             turn = changeTurn()
             audioTurn.play()
             checkWin()
-            document.getElementsByClassName('info')[0].innerText = "Turn for " + turn
+            if (!isgameover) {
+                document.getElementsByClassName('info')[0].innerText = "Turn for " + turn
+            }
         }
     })
 })
